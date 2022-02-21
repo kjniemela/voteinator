ac079926b9f9c9814bd441587da98efb883e5117
Heine574 <johannes@niemela.se>
2022-02-21 12:12:15 -0600

    Changed file extentions to jsx

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
