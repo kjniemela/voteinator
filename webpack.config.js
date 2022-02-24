3db187575bda6195a2969089f7a2859e5e12b348
Heine574 <johannes@niemela.se>
2022-02-23 18:34:07 -0600

    modified file structure to allow github pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
     filename: 'bundle.js'
   },
   module: {
