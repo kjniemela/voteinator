8dc771e0f1a278134cbbbcf0ab6f1f719303bf53
Heine574 <johannes@niemela.se>
2022-02-28 14:03:19 -0600

    added leaderboardItem componend +styles

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import LeaderboardItem from "./LeaderboardItem.jsx";
 
 const Leaderboard = ({ rankList }) => (
   <div className="leaderboard">
-
+    {rankList.map((item, i) => (
+      <LeaderboardItem item={item} key={i} />
+    ))}
   </div>
 );
 
---
+++ src/components/LeaderboardItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeaderboardItem = ({ item }) => (
+  <div className="leaderboardItem">
+    <img src={item.img} alt={item.caption}></img>
+    <p>{item.desc}</p>
+    <p>Rating: {item.score}</p>
+  </div>
+);
+
+export default LeaderboardItem;
